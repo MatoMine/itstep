b4b54fc9e53cc1f15198d21407f1073ad6e67b84
Матвей <alm54966@gmail.com>
2022-04-03 12:50:50 +0200

    First Commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 class Student:
-    def __init__(self):
-        self.heigth = 160
+    amount_of_student=0
+    def __init__(self, height=160):
+        self.height=height
+        Student.amount_of_students+=1
+    def grow(self, height=170):
+        self.height+=height
+nick=Student()
+nick.grow(height=15)
+print(kate.height)
+print(nick.height)
 
-nick = Student()
-print(nick.heigth)
 
